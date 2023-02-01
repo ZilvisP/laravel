36578dc1e69b0ae6114cb66735e521f874171ae2
ZilvisP <zilvinas.paezerinis@gmail.com>
2023-02-01 12:02:18 +0200

    added payement table + model with @properties

---
+++ laravel_failai/app/Models/Payment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $amount
+ * @property int $status_id
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
+
+class Payment extends Model
+{
+    use HasFactory;
+}
